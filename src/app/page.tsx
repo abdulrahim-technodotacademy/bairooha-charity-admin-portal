049e4459f3b1c2bbf0451e4b3e9a16b3a3d1c5c7
Aswin pp <aswin@techno.academy>
2025-07-08 13:25:10 +0530

    logo updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ const PublicHeader = () => (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
         <div className="container flex h-16 items-center justify-between">
             <Link href="/" className="flex items-center space-x-2">
-            <img src="/assets/logo.png" className="h-16 w-14 " alt="logo" />
+            <img src="/assets/logo.png" className="h-16 w-14 ms-3" alt="logo" />
                 {/* <HandCoins className="h-6 w-6 text-primary" /> */}
                 <span className="font-bold text-lg">Bairooha Foundation</span>
             </Link>
